4e3ca2503ba5496b6d19a4eda371b06861ca281e
Elroy Kanye <elroykanye@gmail.com>
2022-07-28 13:45:01 +0100

    add auth interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpResponse
+} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+import {LocalStorageUtil} from "../utils/local-storage.util";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const xAuthToken = LocalStorageUtil.readUserToken();
+    if (!xAuthToken) {
+      return next.handle(request).pipe(
+        tap((event) => {
+          if (event instanceof HttpResponse) {
+            const xAuthToken: string | null = event.headers.get("X-Auth-Token");
+            if (xAuthToken && xAuthToken !== "") {
+              console.log(xAuthToken)
+              LocalStorageUtil.writeUserToken(xAuthToken);
+            }
+          }
+        })
+      );
+    } else {
+      const request1 = request.clone({
+        headers: request.headers.set('X-Auth-Token', xAuthToken)
+      });
+      return next.handle(request1);
+    }
+  }
+}
