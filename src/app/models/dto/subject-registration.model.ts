f09324e211bd50c404f817a5d49898eb7e933490
Elroy Kanye <elroykanye@gmail.com>
2022-07-28 13:54:41 +0100

    replace student id and year id with sat id

--- src/app/models/dto/subject-registration.model.ts
+++ src/app/models/dto/subject-registration.model.ts
@@ -1,7 +1,6 @@
 export interface SubjectRegistration {
   id: number;
-  student_id: number;
-  year_id: number;
+  sat_id: number;
   subject_id: number;
   created_at?: string;
   updated_at?: string;
