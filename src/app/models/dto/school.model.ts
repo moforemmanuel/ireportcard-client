456bcf23b74961c2e70ae336decf1c431922126e
Elroy Kanye <elroykanye@gmail.com>
2022-07-30 20:40:49 +0100

    update the school model to match backend

--- src/app/models/dto/school.model.ts
+++ src/app/models/dto/school.model.ts
@@ -1,5 +1,10 @@
 export interface School {
-  id: number;
-  name: string;
-  number_of_sections?: number;
+  id: number,
+  name: string,
+  curr_year_id?: number,
+  curr_term?: string,
+  curr_seq_id?: number,
+  max_grade: number,
+  application_is_open: boolean,
+  number_of_sections?: number
 }
