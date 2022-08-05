b4521fcda05d4d6a921cf04ca170ecef2cb19d40
Elroy Kanye <elroykanye@gmail.com>
2022-08-05 12:50:21 +0100

    refactor student.component

--- src/app/modules/rc-dashboard/components/rc-students/student/student.component.ts
+++ src/app/modules/rc-dashboard/components/rc-students/student/student.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Student} from "../../../../../models/dto/student.model";
 import {StudentService} from "../../../../../services/student.service";
 import {MessageService} from "primeng/api";
@@ -8,6 +8,7 @@ import {Gender} from "../../../../../models/enum/gender.enum";
 import {DateUtil} from "../../../../../utils/date.util";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {LocalStorageUtil} from "../../../../../utils/local-storage.util";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-student',
@@ -15,67 +16,47 @@ import {LocalStorageUtil} from "../../../../../utils/local-storage.util";
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-  @Input() student: Student;
-  genders: string[] = Object.keys(Gender);
+  student!: Student;
+  genders: Gender[] = [Gender.FEMALE, Gender.MALE, Gender.OTHER];
   studentForm: FormGroup = new FormGroup({});
-  private readonly defaultStudent: Student = {id: -1, name: "", gender: "M", dob: '', pob: "", regNum: "", schoolId: -1};
-
   constructor(
-    private activeModal: NgbActiveModal,
+    private fb: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
     private studentService: StudentService,
-    private messageService: MessageService
+    private msgService: MessageService
   ) {
-    this.student = this.defaultStudent;
+    const studentId = this.activatedRoute.snapshot.params['id'];
+    if (studentId) {
+      this.studentService.getById(studentId).subscribe({
+        next: (res) => {
+          this.student = res;
+          this.setUpStudentForm();
+        },
+        error: () => this.router.navigate(['/dashboard/student']).then()
+      })
+    }
   }
 
   ngOnInit(): void {
-    console.log(this.student)
-    this.setUpStudentForm();
-  }
-
-  resetStudent() {
-    this.student = this.defaultStudent;
-  }
-
-  setUpStudentForm(): void {
-    this.studentForm = new FormGroup({
-      name: new FormControl(this.student.name, Validators.required),
-      gender: new FormControl(this.student.gender, Validators.required),
-      dob: new FormControl(DateUtil.fromRcDate(this.student.dob), Validators.required),
-      pob: new FormControl(this.student.pob, Validators.required),
+    this.studentForm = this.fb.group({
+      name: ['', Validators.required],
+      gender: ['', Validators.required],
+      dob: ['', Validators.required],
+      pob: ['', Validators.required],
     });
   }
 
-  saveStudent() {
-    const schoolId = LocalStorageUtil.readSchoolId();
-    if (schoolId) {
-      const studentToSave: Student = {
-        id: -1,
-        name: this.studentForm.get('name')?.value,
-        regNum: this.studentForm.get('regNum')?.value,
-        gender: this.studentForm.get('gender')?.value,
-        dob: DateUtil.setToRcDateString(this.studentForm.get('dob')?.value),
-        pob: this.studentForm.get('pob')?.value,
-        schoolId: schoolId,
-      };
-
-      if (this.student.id < 0) {
-        this.studentService.save(studentToSave).subscribe({
-          next: (res) => addToMessageService(this.messageService, 'success', 'Success', `${res.message}`),
-          error: (err) => addToMessageService(this.messageService, 'error', 'Error', `${err.message}`)
-        });
-      } else {
-        studentToSave.id = this.student.id;
-        this.studentService.update(studentToSave).subscribe({
-          next: (res) => addToMessageService(this.messageService, 'info', 'Updated', res.message,),
-          error: (err) => addToMessageService(this.messageService, 'error', 'Error', `${err.message}`)
-        })
-      }
-    }
-
+  setUpStudentForm(): void {
+    this.studentForm = this.fb.group({
+      name: [this.student.name, Validators.required],
+      gender: [this.student.gender, Validators.required],
+      dob: [this.student.dob, Validators.required],
+      pob: [this.student.pob, Validators.required],
+    });
   }
 
-  closeModal() {
-    this.activeModal.close();
+  saveStudentAction() {
+
   }
 }
