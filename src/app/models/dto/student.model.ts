40d48ea9133b00f81712b985e6988313a3c13c39
Elroy Kanye <elroykanye@gmail.com>
2022-08-05 12:51:17 +0100

    change studentApiUrl to apiUrl

--- src/app/models/dto/student.model.ts
+++ src/app/models/dto/student.model.ts
@@ -1,7 +1,9 @@
+import {Gender} from "../enum/gender.enum";
+
 export interface Student {
   id: number;
   name: string;
-  gender: string;
+  gender: Gender;
   dob: string;
   pob: string;
   regNum: string;
