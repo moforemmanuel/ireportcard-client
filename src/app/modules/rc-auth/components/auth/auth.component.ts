affafd429a5d044c9f5f219c78a8d6adfa2ed867
Elroy Kanye <elroykanye@gmail.com>
2022-08-14 19:19:18 +0100

    refactor auth.component

--- src/app/modules/rc-auth/components/auth/auth.component.ts
+++ src/app/modules/rc-auth/components/auth/auth.component.ts
@@ -7,8 +7,17 @@ import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  template: `
+    <div>
+      <p-toast></p-toast>
+      <div class="flex justify-content-center">
+        <div class="surface-card p-4 shadow-2 border-round w-full m-2 lg:m-8">
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+    </div>
+  `
 })
 export class AuthComponent implements OnInit {
   login: boolean = true;
@@ -17,21 +26,5 @@ export class AuthComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.authRedirect();
-  }
-
-  authRedirect = () => {
-    const sessionId: string | null = LocalStorageUtil.readUserToken();
-    if (sessionId) {
-      this.defaultService.test().subscribe(() => {
-        addToMessageService(this.msgService, 'warn', 'Logout',
-          `You have to logout before you ${this.login ? 'login' : 'register'}.`)
-        this.router.navigate(['/dashboard']).then((r) => console.log(r));
-      });
-    }
-  }
-
-  toggleLogin = () => {
-    this.login = !this.login;
   }
 }
