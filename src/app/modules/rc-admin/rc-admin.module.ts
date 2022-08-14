9f6253cb6124fb7e118908cf2a9feda29fcffb49
Elroy Kanye <elroykanye@gmail.com>
2022-08-14 19:22:42 +0100

    add admin module

---
+++ src/app/modules/rc-admin/rc-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RcAdminRoutingModule } from './rc-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RcAdminRoutingModule
+  ]
+})
+export class RcAdminModule { }
