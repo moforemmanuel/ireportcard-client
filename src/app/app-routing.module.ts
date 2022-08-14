81bf6cc9206bf92ff203a3c8457f73b66bbc2812
Elroy Kanye <elroykanye@gmail.com>
2022-08-14 19:31:00 +0100

    load modules in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,21 @@ const routes: Routes = [
     children: []
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./modules/rc-auth/rc-auth.module').then(m => m.RcAuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./modules/rc-admin/rc-admin.module').then(m => m.RcAdminModule)
+  },
+  {
+    path: 'student',
+    loadChildren: () => import('./modules/rc-student/rc-student.module').then(m => m.RcStudentModule)
+  },
+  {
     path: 'dashboard',
     loadChildren: () => import('./modules/rc-dashboard/rc-dashboard.module').then(m => m.RcDashboardModule)
   },
-  {path: 'auth', loadChildren: () => import('./modules/rc-auth/rc-auth.module').then(m => m.RcAuthModule)},
   {component: SelectSchoolComponent, path: 'select-school', children: [], canActivate: [AuthGuard]}
 ];
 
