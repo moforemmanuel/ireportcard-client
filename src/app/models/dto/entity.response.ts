ae98081d0e828148db2b6570becdfe1ee84b1ec7
Elroy Kanye <elroykanye@gmail.com>
2022-08-14 19:17:34 +0100

    convert entity response to class

--- src/app/models/dto/entity.response.ts
+++ src/app/models/dto/entity.response.ts
@@ -1,12 +1,8 @@
-export interface EntityResponse {
-  id: number;
-  message: string;
-  log: boolean;
-}
+export class EntityResponse {
+  constructor(
+    public id: number,
+    public message: string,
+    public log: boolean
+  ) {}
 
-export const isEntityResponse = (obj: any): obj is EntityResponse => {
-  if (obj) {
-    return 'id' in obj && 'message' in obj && 'log' in obj;
-  }
-  return false;
 }
