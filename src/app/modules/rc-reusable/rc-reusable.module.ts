5fa39ac1c587767913ebeeeedd4e4b8d1fb8d702
Elroy Kanye <elroykanye@gmail.com>
2022-08-15 15:01:52 +0100

    create reusable and shared modules

---
+++ src/app/modules/rc-reusable/rc-reusable.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RcSharedModule} from "../rc-shared.module";
+import {TopMenuComponent} from "./components/top-menu/top-menu.component";
+import {FooterComponent} from "./components/footer/footer.component";
+
+@NgModule({
+  declarations: [
+    TopMenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RcSharedModule
+  ],
+  exports: [
+    TopMenuComponent,
+    FooterComponent
+  ]
+})
+export class RcReusableModule { }
