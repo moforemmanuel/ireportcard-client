1bf6e039d2033ae73a23fc14eb544d918fb738cb
Elroy Kanye <elroykanye@gmail.com>
2022-08-17 08:16:44 +0100

    declare view application component in rc-reusable.module.ts

--- src/app/modules/rc-reusable/rc-reusable.module.ts
+++ src/app/modules/rc-reusable/rc-reusable.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {RcSharedModule} from "../rc-shared.module";
 import {TopMenuComponent} from "./components/top-menu/top-menu.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import {ViewApplicationComponent} from "./components/view-application/view-application.component";
 
 @NgModule({
   declarations: [
     TopMenuComponent,
-    FooterComponent
+    FooterComponent,
+    ViewApplicationComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import {FooterComponent} from "./components/footer/footer.component";
   ],
   exports: [
     TopMenuComponent,
-    FooterComponent
+    FooterComponent,
+    ViewApplicationComponent
   ]
 })
 export class RcReusableModule {
