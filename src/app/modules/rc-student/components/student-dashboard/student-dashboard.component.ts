769b649bbf4140eb342f3c0c054b821d5ab6eb4a
Elroy Kanye <elroykanye@gmail.com>
2022-08-17 08:15:28 +0100

    update ui of student dashboard

--- src/app/modules/rc-student/components/student-dashboard/student-dashboard.component.ts
+++ src/app/modules/rc-student/components/student-dashboard/student-dashboard.component.ts
@@ -5,9 +5,16 @@ import {MenuItem} from "primeng/api";
   selector: 'app-student-dashboard',
   styleUrls: ['./student-dashboard.component.scss'],
   template: `
-    <app-top-menu [menuItems]="studentMenuItems"></app-top-menu>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
+    <header class="z-1 w-full fixed top-0">
+      <p-toast></p-toast>
+      <app-top-menu [menuItems]="studentMenuItems"></app-top-menu>
+    </header>
+    <main class="z-0 h-full min-h-screen my-2 py-8">
+      <router-outlet></router-outlet>
+    </main>
+    <footer class="z-1">
+      <app-footer></app-footer>
+    </footer>
   `
 })
 export class StudentDashboardComponent implements OnInit {
@@ -27,13 +34,19 @@ export class StudentDashboardComponent implements OnInit {
       {
         label: 'Applications',
         icon: 'pi pi-fw pi-book',
-        routerLink: ['/student/applications'],
+        routerLink: ['/student/application'],
         routerLinkActiveOptions: {exact: true},
       },
       {
         label: 'Subjects',
         icon: 'pi pi-fw pi-list',
         routerLink: ['/student/subjects'],
+        routerLinkActiveOptions: {exact: true},
+      },
+      {
+        label: 'Results',
+        icon: 'pi pi-fw pi-list',
+        routerLink: ['/student/results'],
         routerLinkActiveOptions: {exact: true},
       },
     ];
