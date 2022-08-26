fac725bed48ca5a780515d44119fbe3763409505
Elroy Kanye <elroykanye@gmail.com>
2022-08-26 09:20:23 +0100

    inject subject teacher and teacher services

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -4,39 +4,23 @@ import {environment} from "../environments/environment";
 const RC_SERVER_URL = environment.serverUrl;
 
 export const RC_ACADEMIC_YEAR_API_URL = `${RC_SERVER_URL}/api/academic_year`;
-
 export const RC_AUTH_API_URL = `${RC_SERVER_URL}/api/auth`;
-
 export const RC_AUTH_TEST_API_URL = `${RC_SERVER_URL}/api/auth_test`;
-
 export const RC_CLASS_LEVEL_API_URL = `${RC_SERVER_URL}/api/class_level`;
-
 export const RC_CLASS_LEVEL_SUB_API_URL = `${RC_SERVER_URL}/api/class_level_sub`;
-
 export const RC_CLASS_LIST_API_URL = `${RC_SERVER_URL}/api/class_list`;
-
 export const RC_DEFAULT_API_URL = `${RC_SERVER_URL}/api/default`;
-
 export const RC_GRADE_API_URL = `${RC_SERVER_URL}/api/grade`;
-
 export const RC_REPORT_CARD_API_URL = `${RC_SERVER_URL}/api/rc`;
-
 export const RC_SCHOOL_API_URL = `${RC_SERVER_URL}/api/school`;
-
 export const RC_SECTION_API_URL = `${RC_SERVER_URL}/api/section`;
-
 export const RC_SEQUENCE_API_URL = `${RC_SERVER_URL}/api/sequence`;
-
 export const RC_SETTINGS_API_URL = `${RC_SERVER_URL}/api/settings`;
-
 export const RC_STUDENT_API_URL = `${RC_SERVER_URL}/api/student`;
-
 export const RC_STUDENT_APPLICATION_API_URL = `${RC_SERVER_URL}/api/student_application`;
-
 export const RC_SUBJECT_API_URL = `${RC_SERVER_URL}/api/subject`;
-
 export const RC_SUBJECT_REGISTRATION_API_URL = `${RC_SERVER_URL}/api/subject_registration`;
-
+export const RC_SUBJECT_TEACHER_API_URL = `${RC_SERVER_URL}/api/subject_teacher`;
+export const RC_TEACHER_API_URL = `${RC_SERVER_URL}/api/teacher`;
 export const RC_TERM_API_URL = `${RC_SERVER_URL}/api/term`;
-
 export const RC_USER_API_URL = `${RC_SERVER_URL}/api/user`;
--- src/app/models/dto/grade.model.ts
+++ src/app/models/dto/grade.model.ts
@@ -1,6 +1,19 @@
 export interface Grade {
+  key: GradeKey;
   score: number;
   description: string;
+  graderId: number;
+  histories?: GradeHistory[];
+}
+
+export interface GradeKey {
   registrationId: number;
   sequenceId: number;
+}
+
+export interface GradeHistory {
+  id: number;
+  score: number;
+  createdAt: string;
+  graderId: number;
 }
