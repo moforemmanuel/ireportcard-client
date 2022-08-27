734bede8762a6affb06ce338bd4ebe8336045b5f
Elroy Kanye <elroykanye@gmail.com>
2022-08-27 19:15:31 +0100

    complete impl of admin home

--- src/app/modules/rc-admin/components/admin-home/admin-home.component.ts
+++ src/app/modules/rc-admin/components/admin-home/admin-home.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {User, UserComplete} from "../../../../models/dto/user.model";
+import {UserService} from "../../../../services/user.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Role} from "../../../../models/enum/role.enum";
 
 @Component({
   selector: 'app-admin-home',
@@ -6,11 +10,18 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit {
-
-  constructor() {
+  admin?: UserComplete;
+  constructor(
+    private _fb: FormBuilder,
+    private _userService: UserService
+  ) {
   }
 
   ngOnInit(): void {
+    this.loadUser();
   }
 
+  loadUser = () => this._userService.getCompleteFromSession().subscribe(u => {
+    this.admin = u;
+  });
 }
