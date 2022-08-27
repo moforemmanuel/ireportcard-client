df22ad943d0d5e78f66b76c740c831eba43098d0
Elroy Kanye <elroykanye@gmail.com>
2022-08-27 04:53:56 +0100

    get all user client call added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import {RC_USER_API_URL} from "../app.constants";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User, UserComplete} from "../models/dto/user.model";
+import {EntityResponse} from "../models/dto/entity.response";
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +15,18 @@ export class UserService {
     private http: HttpClient
   ) { }
 
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}`);
+  }
   getComplete(id: number): Observable<UserComplete> {
     return this.http.get<UserComplete>(`${this.apiUrl}/complete/${id}`);
   }
 
   getCompleteFromSession(): Observable<UserComplete> {
     return this.http.get<UserComplete>(`${this.apiUrl}/complete`);
+  }
+
+  toggleApproved(id: number): Observable<EntityResponse> {
+    return this.http.put<EntityResponse>(`${this.apiUrl}/${id}/toggle-approved`, {});
   }
 }
