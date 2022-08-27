ae077a313d37b814ef1bec02b070e195a01ee460
Elroy Kanye <elroykanye@gmail.com>
2022-08-27 08:20:40 +0100

    remove select school from admin routes

--- src/app/modules/rc-admin/rc-admin-routing.module.ts
+++ src/app/modules/rc-admin/rc-admin-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminDashboardComponent} from "./components/admin-dashboard/admin-dashboard.component";
 import {AuthGuard} from "../../guards/auth.guard";
-import {SelectSchoolComponent} from "./components/select-school/select-school.component";
+import {RcSelectSchoolComponent} from "../rc-dashboard/components/rc-select-school/rc-select-school.component";
 import {AdminSettingComponent} from "./components/admin-setting/admin-setting.component";
 import {AdminUsersComponent} from "./components/admin-users/admin-users.component";
 import {AdminHomeComponent} from "./components/admin-home/admin-home.component";
@@ -14,7 +14,6 @@ const routes: Routes = [
     canActivate: [AuthGuard, AuthAdminGuard],
     children: [
       {component: AdminHomeComponent, path: ''},
-      {component: SelectSchoolComponent, path: 'select-school'},
       {component: AdminSettingComponent, path: 'settings'},
       {component: AdminUsersComponent, path: 'users'},
     ]
