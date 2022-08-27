da22699b0d947dbc93e0bd446e27f8c0a181286d
Elroy Kanye <elroykanye@gmail.com>
2022-08-27 04:58:48 +0100

    add component to list subjects in table

--- src/app/modules/rc-student/components/student-subjects/student-subjects.component.spec.ts
+++ src/app/modules/rc-student/components/student-subjects/student-subjects.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { StudentSubjectsComponent } from './student-subjects.component';
+import {StudentSubjectsComponent} from './student-subjects.component';
 
 describe('StudentSubjectsComponent', () => {
   let component: StudentSubjectsComponent;
@@ -8,9 +8,9 @@ describe('StudentSubjectsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StudentSubjectsComponent ]
+      declarations: [StudentSubjectsComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
